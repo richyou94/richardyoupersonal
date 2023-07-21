950eb33d1bd1723bd797316cfe0db9b0e70896d6
Richard You <richardyou94@gmail.com>
2023-07-21 09:00:11 -0700

    adding the react app for my portfolio page

---
+++ richardyoupersonal/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="title-page">
+      <div className="logo-box">
+        <span className="logo-letter logo-upper">R</span>
+        <span className="logo-name">Richard You</span>
+        <span className="logo-letter logo-lower">Y</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ richardyoupersonal/src/components/Logo/BigLogo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BigLogo() {
+  return (
+    <div className="logo-box">
+      <span className="logo-letter logo-upper">R</span>
+      <span className="logo-name">Richard You</span>
+      <span className="logo-letter logo-lower">Y</span>
+    </div>
+  );
+}
+
+export default BigLogo;
