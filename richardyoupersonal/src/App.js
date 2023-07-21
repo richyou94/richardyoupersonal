8d94abfaa634c68dc3255c87ef1fa25c8fab441b
Richard You <richardyou94@gmail.com>
2023-07-21 10:40:42 -0700

    add default container as dark theme in layouts component

--- richardyoupersonal/src/App.js
+++ richardyoupersonal/src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Container from "./components/Layouts/Container";
 import BigLogo from "./components/Logo/BigLogo";
 
 function App() {
   return (
-    <div className="title-page">
+    <Container>
       <BigLogo />
-    </div>
+    </Container>
   );
 }
 
